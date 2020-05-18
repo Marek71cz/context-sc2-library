9de70be2aeb82d2a08b3ecf4f189ee461032be88
Marek71cz <marek.71cz@gmail.com>
2020-05-18 17:42:08 +0200

    Added check for settings

--- addon.py
+++ addon.py
@@ -5,36 +5,44 @@ import xbmcvfs
 import os
  
 addon = xbmcaddon.Addon()
-mf = addon.getSetting("movielFolder")
-tf = addon.getSetting("tvshowsFolder")
 
-# ToDo:
-#if mf == '':
-    #setLibraryPath()
+def setLibraryPath():
+    addon.openSettings()
 
-# create directory - original title and year
-originalTitle   = xbmc.getInfoLabel("ListItem.Originaltitle").decode('utf-8')
-year            = xbmc.getInfoLabel("ListItem.Year").decode('utf-8')
-moviePath       = xbmc.getInfoLabel("ListItem.FileNameAndPath")
-dbtype          = xbmc.getInfoLabel("ListItem.dbtype")
-tvshowtitle     = xbmc.getInfoLabel("Container.ShowTitle")
+def addMovie():
+    # create directory - original title and year
+    originalTitle   = xbmc.getInfoLabel("ListItem.Originaltitle").decode('utf-8')
+    year            = xbmc.getInfoLabel("ListItem.Year").decode('utf-8')
+    moviePath       = xbmc.getInfoLabel("ListItem.FileNameAndPath")
+    dbtype          = xbmc.getInfoLabel("ListItem.dbtype")
+    tvshowtitle     = xbmc.getInfoLabel("Container.ShowTitle")
 
-if dbtype == 'movie':
-    dirName = mf + originalTitle + ' (' + year + ')'
+    if dbtype == 'movie':
+        dirName = mf + originalTitle + ' (' + year + ')'
+    # ToDo: Need to implement TV Shows
+    else:
+        return
 
-if dbtype == 'tvshow':
-    dirName = tf + tvshowtitle + ' (' + year + ')'
+    #if dbtype == 'tvshow':
+    #    dirName = tf + tvshowtitle + ' (' + year + ')'
 
-xbmcgui.Dialog().ok(dbtype, dirName)
+    # xbmcgui.Dialog().ok(dbtype, dirName)
 
-if not xbmcvfs.exists(dirName):
-    xbmcvfs.mkdir(dirName)
+    if not xbmcvfs.exists(dirName):
+        xbmcvfs.mkdir(dirName)
     strmFileName    = os.path.join(dirName, originalTitle + ' (' + year + ')' + '.strm')
     file            = xbmcvfs.File(strmFileName, 'w')
     file.write(str(moviePath))
     file.close()
     xbmc.executebuiltin('UpdateLibrary(video)')
-    
 
-    
+# Check settings, whether library paths are set up corretly
+if addon.getSetting("movielFolder") == '':
+    setLibraryPath()
 
+mf = addon.getSetting("movielFolder")
+tf = addon.getSetting("tvshowsFolder")
+
+if mf != '':
+    addMovie()
+       
