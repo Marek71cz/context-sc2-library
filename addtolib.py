f6ff9ee4fa87ad99c7434992a2641047496e01e3
Marek71cz <marek.71cz@gmail.com>
2020-05-22 09:52:20 +0200

    Some code changes, no functionality changes

--- addtolib.py
+++ addtolib.py
@@ -7,7 +7,7 @@ import os
 addon   = xbmcaddon.Addon()
 dbtype  = xbmc.getInfoLabel("ListItem.dbtype")
 
-def addMovie():
+def addMovie(mf):
     originalTitle   = xbmc.getInfoLabel("ListItem.Originaltitle").decode('utf-8')
     year            = xbmc.getInfoLabel("ListItem.Year").decode('utf-8')
     moviePath       = xbmc.getInfoLabel("ListItem.FileNameAndPath")
@@ -22,31 +22,35 @@ def addMovie():
     file.close()
     xbmc.executebuiltin('UpdateLibrary(video)')
 
-def addTVShow():
+def addTVShow(tf):
     # ToDo:
     originalTitle   = xbmc.getInfoLabel("ListItem.TVShowTitle").decode('utf-8')
-    tvShowTitle     = xbmc.getInfoLabel("ListItem.Title")
+    tvShowTitle     = xbmc.getInfoLabel("ListItem.Title").decode('utf-8')
     year            = xbmc.getInfoLabel("ListItem.Year").decode('utf-8')
     tvShowPath      = xbmc.getInfoLabel("ListItem.FileNameAndPath")
     seasonsCount    = xbmc.getInfoLabel("ListItem.Property('TotalSeasons')")
 
+    # ToDo: Create Tv Show directory and season subdirectories with episode links
     dirName = tf + tvShowTitle + ' (' + year + ')'
     # xbmcgui.Dialog().ok(dbtype, 'Original title: ' + originalTitle, 'TV Show title: ' + tvShowTitle, 'Seasons count: ' + seasonsCount)
 
-    # ToDo: Create Tv Show directory and season subdirectories with episode links
 
-# Check settings, whether library paths are set up corretly
-if dbtype == 'movie':
-    if addon.getSetting("movielFolder") == '':
-        addon.openSettings()
-    mf = addon.getSetting("movielFolder")
-    if mf != '':
-        addMovie()
+def addItem():
+    # Check settings, whether library paths are set up corretly
+    if dbtype == 'movie':
+        if addon.getSetting("movielFolder") == '':
+            addon.openSettings()
+        mf = addon.getSetting("movielFolder")
+        if mf != '':
+            addMovie(mf)
 
-if dbtype == 'tvshow':
-    xbmcgui.Dialog().ok(addon.getLocalizedString(30030), addon.getLocalizedString(30031))
-    #if addon.getSetting("tvshowsFolder") == '':
-    #    addon.openSettings()
-    #tf = addon.getSetting("tvshowsFolder")
-    #if tf != '':
-    #    addTVShow()
+    if dbtype == 'tvshow':
+        xbmcgui.Dialog().ok(addon.getLocalizedString(30030), addon.getLocalizedString(30031))
+        #if addon.getSetting("tvshowsFolder") == '':
+        #    addon.openSettings()
+        #tf = addon.getSetting("tvshowsFolder")
+        #if tf != '':
+        #    addTVShow(tf)
+        
+        
+addItem()
